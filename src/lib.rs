4448107b7338cfc5ccad5a60a2275137e64af5a3
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2024-11-24 22:37:37 +0000

    s02c15: Implement checked pkcs7 unpad

--- src/lib.rs
+++ src/lib.rs
@@ -24,3 +24,4 @@ pub use set02::c11::{aes_encryption_oracle, random_bytes, EncryptionMode};
 pub use set02::c12::{byte_at_a_time_aes_ecb_decrypt, EcbOracle};
 pub use set02::c13::{falsify_admin_account_with_ecb_oracle, UserProfile, UserProfileOracle};
 pub use set02::c14::{random_prefix_byte_at_a_time_with_aes_ecb_decrypt, EcbRandomPrefixOracle};
+pub use set02::c15::pkcs7_unpad;
--- src/set02/c15.rs
+++ src/set02/c15.rs
@@ -1,3 +1,50 @@
 /// PKCS#7 padding validation
 
-pub fn pkcs7_unpad(bytes: &[u8]) -> bool {}
+pub fn pkcs7_unpad(bytes: &mut Vec<u8>) -> bool {
+    if let Some(n_pad) = is_pkcs7_padded(bytes) {
+        bytes.truncate(bytes.len() - n_pad as usize);
+        return true;
+    }
+    false
+}
+
+fn is_pkcs7_padded(bytes: &[u8]) -> Option<u8> {
+    if let Some(n_pad) = bytes.last() {
+        if *n_pad as usize > bytes.len() {
+            return None;
+        }
+        let padded = &bytes[(bytes.len() - *n_pad as usize)..];
+        if padded.iter().all(|el| el == n_pad) {
+            return Some(*n_pad);
+        }
+    }
+    None
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[test]
+    fn pkcs7_pad_pads_message() {
+        let mut msg = b"ICE ICE BABY\x04\x04\x04\x04".to_vec();
+
+        let unpadded = pkcs7_unpad(&mut msg);
+
+        assert!(unpadded);
+        assert_eq!(msg, b"ICE ICE BABY");
+    }
+
+    #[rstest]
+    #[case("ICE ICE BABY\x05\x05\x05\x05")]
+    #[case("ICE ICE BABY\x01\x02\x03\x04")]
+    fn pkcs7_unpad_unpads_message(#[case] padded: &str) {
+        let mut msg = padded.as_bytes().to_vec();
+
+        let unpadded = pkcs7_unpad(&mut msg);
+
+        assert!(!unpadded);
+    }
+}
