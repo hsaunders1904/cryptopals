d60ce7899f317942e1013cc256c84d6cd8e33fe2
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2025-05-20 23:07:18 +0100

    s05c35: Reduce code duplication

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,4 @@ pub use set04::c32::hmac_sha1_timing_attack_with_rounds;
 
 pub use set05::c33::{generate_modexp_keypair, ModExpKeyPair};
 pub use set05::c34::{simulate_dh_parameter_injection_attack, Person};
-pub use set05::c35::{
-    simulate_dh_g_eq_1_injection_attack, simulate_dh_g_eq_p_injection_attack,
-    simulate_dh_g_eq_p_minus_1_injection_attack,
-};
+pub use set05::c35::simulate_dh_g_injection_attack;
--- src/set05/c35.rs
+++ src/set05/c35.rs
@@ -4,82 +4,80 @@ use crate::{decrypt_aes_128_cbc, Person, Sha1};
 
 use num_bigint::BigUint;
 
-pub fn simulate_dh_g_eq_1_injection_attack(
-    alice: &mut Person,
-    bob: &mut Person,
-) -> Result<Vec<u8>, String> {
-    // Alice sends 'p' and 'g' to Bob. However, the attacker intercepts these
-    // and forwards a spoofed 'g' on to Bob.
-    let spoofed_g = BigUint::from(1u64);
-    bob.recv_g(spoofed_g);
-
-    // Alice and Bob then exchange public keys and generate a session AES key.
-    // Note that because Alice and Bob have different values for g, they
-    // will not generate the same session key, so will not be able to
-    // communicate.
-    let bob_pub_key = bob.public_key();
-
-    // Consider the derivation of Alice's session key, let 'a' be Alice's private
-    // key, and 'B' be Bob's public key:
-    //  B = g ^ a mod p, where we've set g := 1
-    //    = 1
-    // Alice's session key 's' is derived using:
-    //  s = SHA1(B ^ a mod p)[0:16]
-    //    = SHA1(1)[0:16]
-
-    // Alice sends an encrypted message to Bob.
-    let alices_message = alice.encrypt_message(&bob_pub_key);
-
-    // The attacker can intercept this and decrypt it.
-    let forced_key: [u8; 16] = Sha1::digest_message(&[1])[..16].try_into().unwrap();
-    let alice_recovered_message =
-        decrypt_aes_128_cbc(&alices_message.msg, &forced_key, &alices_message.iv)?;
-
-    Ok(alice_recovered_message)
+pub enum MaliciousG {
+    One,       // g = 1
+    P,         // g = p
+    PMinusOne, // g = p - 1
 }
 
-pub fn simulate_dh_g_eq_p_injection_attack(
-    alice: &mut Person,
-    bob: &mut Person,
-) -> Result<Vec<u8>, String> {
-    // Alice sends 'p' and 'g' to Bob. However, the attacker intercepts these
-    // and forwards a spoofed 'g' on to Bob.
-    let spoofed_g = alice.p();
-    bob.recv_g(spoofed_g);
+impl MaliciousG {
+    fn value_from_p(&self, p: &BigUint) -> BigUint {
+        match self {
+            MaliciousG::One => BigUint::from(1u64),
+            MaliciousG::P => p.clone(),
+            MaliciousG::PMinusOne => p - 1u64,
+        }
+    }
 
-    // Alice and Bob then exchange public keys and generate a session AES key.
-    // Note that because Alice and Bob have different values for g, they
-    // will not generate the same session key, so will not be able to
-    // communicate.
-    let bob_pub_key = bob.public_key();
-
-    // Consider the derivation of Alice's session key, let 'a' be Alice's private
-    // key, and 'B' be Bob's public key:
-    //  B = g ^ a mod p, where we've set g := p
-    //    = p ^ a mod p
-    //    = 0
-    // Alice's session key 's' is derived using:
-    //  s = SHA1(0 ^ a mod p)[0:16]
-    //    = SHA1(0)[0:16]
-
-    // Alice sends an encrypted message to Bob.
-    let alices_message = alice.encrypt_message(&bob_pub_key);
-
-    // The attacker can intercept this and decrypt it.
-    let forced_key: [u8; 16] = Sha1::digest_message(&[0])[..16].try_into().unwrap();
-    let alice_recovered_message =
-        decrypt_aes_128_cbc(&alices_message.msg, &forced_key, &alices_message.iv)?;
-
-    Ok(alice_recovered_message)
+    fn possible_shared_secrets(&self, p: &BigUint) -> Vec<Vec<u8>> {
+        // Here we enumerate what the forced shared secrets given our value of
+        // g.
+        // Consider the derivation of Alice's shared secret, let 'a' be Alice's
+        // private key, and 'B' and 'b' be Bob's public and private keys:
+        //   s = B ^ a mod p, where
+        //   B = g ^ b mod p
+        match self {
+            MaliciousG::One => {
+                // We've set g := 1, hence
+                //    B = 1 ^ b mod p
+                //      = 1
+                // Hence, Alice's shared secret 's' is derived using:
+                //  s = B ^ a mod p
+                //    = 1
+                vec![vec![1]]
+            }
+            MaliciousG::P => {
+                // We've set g := p, hence
+                //    B = p ^ b mod p
+                //      = 0
+                // Hence, Alice's shared secret 's' is derived using:
+                //  s = B ^ a mod p
+                //    = 0
+                vec![vec![0]]
+            }
+            MaliciousG::PMinusOne => {
+                // We've set g = p - 1, hence
+                //   B = (p - 1) ^ b mod p    (1)
+                //
+                // Note the identity:
+                //
+                //   (p − 1) ≡ (-1) mod p
+                //   => (p - 1) ^ a ≡ ((-1) ^ a) mod p
+                //
+                // Hence, from (1):
+                //
+                //   B = ((-1) ^ b) mod p
+                //     =  p - 1 if b is odd
+                //     =  1     if b is even
+                //
+                // Therefore
+                //   s = (1 ^ a) mod p       = 1
+                // or
+                //   s = ((p - 1) ^ a) mod p = p - 1
+                vec![vec![1], (p - 1u64).to_bytes_be()]
+            }
+        }
+    }
 }
 
-pub fn simulate_dh_g_eq_p_minus_1_injection_attack(
+pub fn simulate_dh_g_injection_attack(
     alice: &mut Person,
     bob: &mut Person,
+    malicious_g: MaliciousG,
 ) -> Result<Vec<u8>, String> {
     // Alice sends 'p' and 'g' to Bob. However, the attacker intercepts these
     // and forwards a spoofed 'g' on to Bob.
-    let spoofed_g = alice.p() - 1u64;
+    let spoofed_g = malicious_g.value_from_p(&alice.p());
     bob.recv_g(spoofed_g);
 
     // Alice and Bob then exchange public keys and generate a session AES key.
@@ -88,47 +86,20 @@ pub fn simulate_dh_g_eq_p_minus_1_injection_attack(
     // communicate.
     let bob_pub_key = bob.public_key();
 
-    // Consider the derivation of Alice's session key, let 'a' be Alice's
-    // private key, and 'B' be Bob's public key:
-    //
-    //   B = g ^ a mod p, where we've set g := p - 1
-    //     = (p - 1) ^ a mod p    (1)
-    //
-    // Note the identity:
-    //
-    //   (p − 1) ≡ (-1) mod p
-    //   => (p - 1) ^ a ≡ ((-1) ^ a) mod p
-    //
-    // Hence, from (1):
-    //
-    //   B = ((-1) ^ a) mod p
-    //     =  p - 1 if a is odd
-    //     =  1     if a is even
-
-    // Therefore, Alice's session key 's' is derived using either
-    //   s = SHA1(p - 1)[0:16]
-    // or
-    //   s = SHA1(1)[0:16]
-
     // Alice sends an encrypted message to Bob.
     let alices_message = alice.encrypt_message(&bob_pub_key);
 
-    // The attacker can intercept this and decrypt it.
-    let forced_key: [u8; 16] = Sha1::digest_message(&(alice.p() - 1u64).to_bytes_le())[..16]
-        .try_into()
-        .unwrap();
-    if let Ok(recovered_msg) =
-        decrypt_aes_128_cbc(&alices_message.msg, &forced_key, &alices_message.iv)
-    {
-        return Ok(recovered_msg);
+    // The attacker can intercept the message and decrypt it.
+    for shared_secret in malicious_g.possible_shared_secrets(&alice.p()) {
+        let digest = Sha1::digest_message(&shared_secret);
+        let aes_key: [u8; 16] = digest[..16].try_into().unwrap();
+        if let Ok(decrypted) =
+            decrypt_aes_128_cbc(&alices_message.msg, &aes_key, &alices_message.iv)
+        {
+            return Ok(decrypted);
+        }
     }
-    let forced_key2: [u8; 16] = Sha1::digest_message(&[1])[..16].try_into().unwrap();
-    if let Ok(recovered_msg) =
-        decrypt_aes_128_cbc(&alices_message.msg, &forced_key2, &alices_message.iv)
-    {
-        return Ok(recovered_msg);
-    }
-    Err("failed to recover message".to_string())
+    Err("Failed to recover message".to_string())
 }
 
 #[cfg(test)]
@@ -154,74 +125,56 @@ mod tests {
         .unwrap()
     });
 
-    #[test]
-    fn simulate_dh_g_eq_1_injection_attack_recovers_message() {
-        let default_g = BigUint::from(2u64);
-        let mut alice = Person::new(
-            Mt19937::new(101),
+    fn setup_people() -> (Person, Person) {
+        let g = BigUint::from(2u64);
+        let alice = Person::new(
+            Mt19937::new(2),
             "Vive la resistance".to_string(),
             P.clone(),
-            default_g.clone(),
+            g.clone(),
         );
-        let mut bob = Person::new(
-            Mt19937::new(101),
+        let bob = Person::new(
+            Mt19937::new(3),
             "Vive la France".to_string(),
             P.clone(),
-            default_g.clone(),
+            g.clone(),
         );
+        (alice, bob)
+    }
 
-        let a = simulate_dh_g_eq_1_injection_attack(&mut alice, &mut bob).unwrap();
+    #[test]
+    fn simulate_dh_g_eq_1_injection_attack_recovers_message() {
+        let (mut alice, mut bob) = setup_people();
+
+        let a_msg = simulate_dh_g_injection_attack(&mut alice, &mut bob, MaliciousG::One).unwrap();
 
         assert_eq!(
-            String::from_utf8_lossy(&a),
+            String::from_utf8_lossy(&a_msg),
             "Vive la resistance".to_string(),
         );
     }
 
     #[test]
     fn simulate_dh_g_eq_p_injection_attack_recovers_message() {
-        let default_g = BigUint::from(2u64);
-        let mut alice = Person::new(
-            Mt19937::new(101),
-            "Vive la resistance".to_string(),
-            P.clone(),
-            default_g.clone(),
-        );
-        let mut bob = Person::new(
-            Mt19937::new(101),
-            "Vive la France".to_string(),
-            P.clone(),
-            default_g.clone(),
-        );
+        let (mut alice, mut bob) = setup_people();
 
-        let a = simulate_dh_g_eq_p_injection_attack(&mut alice, &mut bob).unwrap();
+        let a_msg = simulate_dh_g_injection_attack(&mut alice, &mut bob, MaliciousG::P).unwrap();
 
         assert_eq!(
-            String::from_utf8_lossy(&a),
+            String::from_utf8_lossy(&a_msg),
             "Vive la resistance".to_string(),
         );
     }
 
     #[test]
     fn simulate_dh_g_eq_p_minus_1_injection_attack_recovers_message() {
-        let default_g = BigUint::from(2u64);
-        let mut alice = Person::new(
-            Mt19937::new(101),
-            "Vive la resistance".to_string(),
-            P.clone(),
-            default_g.clone(),
-        );
-        let mut bob = Person::new(
-            Mt19937::new(101),
-            "Vive la France".to_string(),
-            P.clone(),
-            default_g.clone(),
-        );
+        let (mut alice, mut bob) = setup_people();
 
-        let a = simulate_dh_g_eq_p_minus_1_injection_attack(&mut alice, &mut bob).unwrap();
+        let a_msg =
+            simulate_dh_g_injection_attack(&mut alice, &mut bob, MaliciousG::PMinusOne).unwrap();
 
         assert_eq!(
-            String::from_utf8_lossy(&a),
+            String::from_utf8_lossy(&a_msg),
             "Vive la resistance".to_string(),
         );
     }
